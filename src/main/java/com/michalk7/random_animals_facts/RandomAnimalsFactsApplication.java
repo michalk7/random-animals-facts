47821572b6cd076eca67a0a017090090896d8e8f
Michał Kubara <michalkubara7@gmail.com>
2021-12-04 20:19:18 +0100

    Add generated project files.

---
+++ src/main/java/com/michalk7/random_animals_facts/RandomAnimalsFactsApplication.java
@@ -0,0 +1,13 @@
+package com.michalk7.random_animals_facts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomAnimalsFactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomAnimalsFactsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/michalk7/random_animals_facts/RandomAnimalsFactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalk7.random_animals_facts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomAnimalsFactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
