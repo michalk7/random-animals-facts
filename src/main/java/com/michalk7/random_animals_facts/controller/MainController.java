c80055c7401d1dc0cae256628333e02074a5252a
Michał Kubara <michalkubara7@gmail.com>
2021-12-06 19:30:46 +0100

    Change home page html file name.

--- src/main/java/com/michalk7/random_animals_facts/controller/MainController.java
+++ src/main/java/com/michalk7/random_animals_facts/controller/MainController.java
@@ -4,8 +4,6 @@ import com.michalk7.random_animals_facts.dto.GeneratorParametersDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -14,7 +12,7 @@ public class MainController {
     public String mainPage(Model model) {
         GeneratorParametersDTO generatorParameters = new GeneratorParametersDTO();
         model.addAttribute("generatorParameters", generatorParameters);
-        return "main-page";
+        return "home";
     }
 
 }
